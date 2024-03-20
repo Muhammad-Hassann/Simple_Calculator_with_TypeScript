9e07f304ffd17189e85575e89028389bf40b153d
Muhammad Hassan <muhammadhassan202005@gmail.com>
2024-03-21 04:43:01 +0500

    Modify indes.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ let answer = await inquirer.prompt([
     { message: "Enter first number", type: "number", name: "firstNumber" },
     { message: "Enter second number", type: "number", name: "secondNumber" },
     {
-        message: "Select an operator for operation",
+        message: "Select an operator to perform operation",
         type: "list",
         name: "operators",
         choices: ["Addition", "Substraction", "Multiplication", "Division"]
@@ -18,6 +18,9 @@ else if (answer.operators === "Substraction") {
 else if (answer.operators === "Multiplication") {
     console.log(answer.firstNumber * answer.secondNumber);
 }
-else {
+else if (answer.operators === "Division") {
     console.log(answer.firstNumber / answer.secondNumber);
+}
+else {
+    console.log("Please select an opertor");
 }
